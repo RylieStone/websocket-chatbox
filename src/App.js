65f48f5b6e4de4b7b1ac4c97803c96132d66c74f
Rylie <rylie.stone2005@gmail.com>
2025-03-26 17:14:11 -0400

    updated app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+// import Chat from './components/Chat';
+// import Main from './components/Main'
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App-header">
+        Chatting area
+      </h1>
+      <Routes>
+        {/* <Route path='/' element={}/> */}
+        {/* <Route path='/chat' element={}/> */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
