892f91bb4c338a6afbf28e8bfa3f63778813a81a
Rylie <rylie.stone2005@gmail.com>
2025-03-27 08:18:57 -0400

    added username for integrated use

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+import React, {useState} from 'react';
 import { Routes, Route } from 'react-router-dom';
-// import Chat from './components/Chat';
-// import Main from './components/Main'
+// import Chat from './components/Chat.js';
+import Main from './components/Main.js'
 function App() {
+  const [username, setUsername] = useState('')
+  const ws = new WebSocket("ws://localhost:4000")
   return (
     <div className="App">
       <h1 className="App-header">
         Chatting area
       </h1>
       <Routes>
-        {/* <Route path='/' element={}/> */}
+        <Route path='/' element={<Main updateUser={setUsername}/>}/>
         {/* <Route path='/chat' element={}/> */}
       </Routes>
     </div>
