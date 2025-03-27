4e78aa5b2bb3b8b390184bb606c25ec0811ef5d7
Rylie <rylie.stone2005@gmail.com>
2025-03-27 08:18:34 -0400

    updated server fixxing bug

--- backend/server.cjs
+++ backend/server.cjs
@@ -1,8 +1,11 @@
-const db = require("../data/dbconfig.cjs")
-const Websocket = require('ws')
+const WebSocket = require('ws')
 
-const wss = new Websocket.Server({ port: 4000 })
+const wss = new WebSocket.Server({ port: 4000 })
 
 wss.on("connection", ws => {
-    console.log("client has connected")
+    console.log("new client has connected to the server")
+
+    ws.on('close', () => {
+        console.log("client has disconnected from the server")
+    })
 })
