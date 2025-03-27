ddd99fcf3258a37c6f316a41fa3381742178abfb
Rylie <rylie.stone2005@gmail.com>
2025-03-27 09:42:35 -0400

    server bugs patched

--- backend/server.cjs
+++ backend/server.cjs
@@ -4,7 +4,11 @@ const wss = new WebSocket.Server({ port: 4000 })
 
 wss.on("connection", ws => {
     console.log("new client has connected to the server")
-
+    ws.on('message', data => {
+        const str = data.toString('utf-8')
+        const obj = JSON.parse(str)
+        console.log(obj)
+    })
     ws.on('close', () => {
         console.log("client has disconnected from the server")
     })
