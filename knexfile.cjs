f39396d0308eb1bf7d3c08f66f0ab697be8c4d5c
Rylie <rylie.stone2005@gmail.com>
2025-03-29 10:30:10 -0400

    updated knex fixxing db bug

--- knexfile.cjs
+++ knexfile.cjs
@@ -1,7 +1,10 @@
 const sharedConfig = {
   client: 'sqlite3',
   useNullAsDefault: true,
-  migrations: { directory: './data/migrations' },
+  migrations: { 
+    directory: './data/migrations',
+    extension: 'cjs'
+  },
   pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
 }
 
