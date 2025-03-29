640999b23cf04408a853a231aabc9aa0ef610d56
Rylie <rylie.stone2005@gmail.com>
2025-03-29 10:31:41 -0400

    updated db config for better integrations

--- data/dbconfig.cjs
+++ data/dbconfig.cjs
@@ -1,6 +1,11 @@
 // do not make changes to this file
 const knex = require('knex');
+const path = require('path')
 const knexConfig = require('../knexfile.cjs');
 const environment = process.env.NODE_ENV || 'development';
+
+if (knexConfig[environment].connection.filename) {
+    knexConfig[environment].connection.filename = path.join(__dirname, '..', knexConfig[environment].connection.filename);
+}
 
 module.exports = knex(knexConfig[environment]);
