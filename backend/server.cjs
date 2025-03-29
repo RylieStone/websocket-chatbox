c7b70c70748983b1be492724e516764fbc988a52
Rylie <rylie.stone2005@gmail.com>
2025-03-29 10:31:27 -0400

    revamped and debugged server

--- backend/server.cjs
+++ backend/server.cjs
@@ -1,15 +1,28 @@
 const WebSocket = require('ws')
-
+const {message, history} = require('./chatModel.cjs')
 const wss = new WebSocket.Server({ port: 4000 })
 
-wss.on("connection", ws => {
-    console.log("new client has connected to the server")
-    ws.on('message', data => {
+let arr = [
+    {id: 1, username: 'bro', message: "yo this is so cool"},
+    {id: 2, username: 'firewall', message: "IKR"},
+    {id: 3, username: 'chatbot', message: "haha"},
+    {id: 4, username: 'typer', message: "man i love this"},
+]
+
+wss.on("connection", async ws => {
+    console.log('client has connected')
+    const data = await history()
+    ws.send(JSON.stringify(data))
+    ws.on('message', async data => {
         const str = data.toString('utf-8')
         const obj = JSON.parse(str)
-        console.log(obj)
+        await message(obj)
 
-        ws.send(JSON.stringify(obj))
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(obj))
+            }
+        })
     })
     ws.on('close', () => {
         console.log("client has disconnected from the server")
